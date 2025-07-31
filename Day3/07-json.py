936c86b92e54d1e7253fd66fe8d9bc6010b9acb1
Donggeol Lee <ippppo7568@gmail.com>
2025-07-31 10:25:38 +0900

    Add melon search and top100 webapp scripts

---
+++ Day3/07-json.py
@@ -0,0 +1,25 @@
+import json
+import requests
+
+url = "https://pyhub.kr/melon/20231204.json"
+
+response = requests.get(url)
+print(response)
+
+# string -> object : deserialize (역직렬화)
+json_string: str = response.text
+response_obj = json.loads(json_string)
+print(type(response_obj))
+print(len(response_obj))
+
+
+
+def bring_info(response_obj):
+    for song in response_obj:
+        yield song
+        
+
+res = bring_info(response_obj)
+
+for i in range(3):
+    print(next(res))
